72ffe5590606abda4caf6815c9146af8f97ad4d0
Rotem Shapira <rotem.rs@gmail.com>
2025-08-20 00:54:16 +0300

    registration: enabling uploading bigger pictures

--- server/index.js
+++ server/index.js
@@ -15,8 +15,22 @@ const pool = require("./db");
 app.use(helmet());
 app.use(cors());
 app.use(morgan("combined"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// הגדלת מגבלת גוף הבקשה כדי לאפשר Base64 של תמונה
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
+// (רשות) הודעה ידידותית אם הגוף גדול מדי
+app.use((err, req, res, next) => {
+  if (err && err.type === "entity.too.large") {
+    return res
+      .status(413)
+      .json({ error: "Image too large. Try a smaller one." });
+  }
+  next(err);
+});
 
 //RONTEST
 
