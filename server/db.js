55785755d579552f4ab0504bd43a2eab20ddb722
Rotem Shapira <rotem.rs@gmail.com>
2025-08-19 14:50:23 +0300

    updating db.js with no password in the file

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 // server/db.js
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'ron12345',
-  database: process.env.DB_NAME || 'mentorship_db',
-  connectionLimit: 10
+  host: process.env.DB_HOST || "127.0.0.1",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "mentorship_db",
+  connectionLimit: 10,
 });
 
 module.exports = pool;
